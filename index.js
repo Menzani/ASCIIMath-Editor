7fa06e1b75a69a773287d63fa2313665197f2d7b
Francesco Menzani <francesco@menzani.eu>
2017-11-20 22:46:31 +0100

    Update license link in homepage

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-// ASCIIMath Editor | (C) 2017 Francesco Menzani – francescomenzani99@gmail.com | https://goo.gl/L1rTj9
+// ASCIIMath Editor | (C) 2017 Francesco Menzani – francescomenzani99@gmail.com | https://github.com/MrSystem/ASCIIMath-Editor/blob/production/LICENSE
 "use strict"
