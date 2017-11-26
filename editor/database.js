6c55f8e71570d06fd0d5d9cfc953e27fe93413d0
Francesco Menzani <francesco@menzani.eu>
2017-11-26 22:57:33 +0100

    Refactor database.js; implement deleteDatabase(); fix undefined reference in upgradeSchema()

--- editor/database.js
+++ editor/database.js
@@ -1,39 +1,51 @@
 // ASCIIMath Editor | (C) 2017 Francesco Menzani – francescomenzani99@gmail.com | https://www.gnu.org/licenses/agpl-3.0.txt
 "use strict"
 
+const DATABASE_NAME = "ASCIIMath Editor"
+const SCHEMA_VERSION = 1
+const DOCUMENT_OBJECT_STORE_NAME = "document"
+const PAGE_OBJECT_STORE_NAME = "page"
+const PAGE_SOURCE_OBJECT_STORE_NAME = "page_source"
+
 let dataSource
 
-function initializeDataModel() {
-    let request = window.indexedDB.open("ASCIIMath Editor", 1)
-    request.onerror = function() {
+function openDatabase() {
+    let request = indexedDB.open(DATABASE_NAME, SCHEMA_VERSION)
+    request.onerror = function () {
         notifyError("We are unable to save your data in Firefox.")
     }
-    request.onsuccess = function() {
+    request.onupgradeneeded = upgradeSchema
+    request.onsuccess = function () {
         dataSource = request.result
         dataSource.onerror = function () {
             notifyError("Some changes may not be saved.", dataSource.errorCode)
         }
     }
-    request.onupgradeneeded = upgradeSchema
 }
 
 // See DB_SCHEMA.md for more information
 function upgradeSchema(event) {
+    let dataSource = event.target.result
     if (event.oldVersion < 1) {
-        dataSource.createObjectStore("document", {autoIncrement: true})
-        dataSource.createObjectStore("page", {autoIncrement: true})
-        dataSource.createObjectStore("page_source", {autoIncrement: true})
+        dataSource.createObjectStore(DOCUMENT_OBJECT_STORE_NAME, {autoIncrement: true})
+        dataSource.createObjectStore(PAGE_OBJECT_STORE_NAME, {autoIncrement: true})
+        dataSource.createObjectStore(PAGE_SOURCE_OBJECT_STORE_NAME, {autoIncrement: true})
     }
 }
 
-function documentStore(level) {
-    return dataSource.transaction("document", level).objectStore("document")
+function documents(level) {
+    return dataSource.transaction(DOCUMENT_OBJECT_STORE_NAME, level).objectStore(DOCUMENT_OBJECT_STORE_NAME)
 }
 
-function pageStore(level) {
-    return dataSource.transaction("page", level).objectStore("page")
+function pages(level) {
+    return dataSource.transaction(PAGE_OBJECT_STORE_NAME, level).objectStore(PAGE_OBJECT_STORE_NAME)
 }
 
-function pageSourceStore(level) {
-    return dataSource.transaction("page_source", level).objectStore("page_source")
+function pageSources(level) {
+    return dataSource.transaction(PAGE_SOURCE_OBJECT_STORE_NAME, level).objectStore(PAGE_SOURCE_OBJECT_STORE_NAME)
+}
+
+function deleteDatabase() {
+    indexedDB.deleteDatabase(DATABASE_NAME)
+    location.reload(true)
 }
