22fcce5a49dcf1ae52e021ede0fcee695e6c3ef6
Francesco Menzani <francesco@menzani.eu>
2017-12-03 19:28:03 +0100

    Shorten copyright notice (ASCIIMath modifications are not sublicensed)

--- editor/lib/ASCIIMathML.js
+++ editor/lib/ASCIIMathML.js
@@ -1,39 +1,16 @@
 /*
-ASCIIMathML.js
-==============
-This file contains JavaScript functions to convert ASCII math notation
-and (some) LaTeX to Presentation MathML. The conversion is done while the
-HTML page loads, and should work with Firefox and other browsers that can
-render MathML.
-
-Just add the next line to your HTML page with this file in the same folder:
-
-<script type="text/javascript" src="ASCIIMathML.js"></script>
-
-Version 2.2 Mar 3, 2014.
-Latest version at https://github.com/mathjax/asciimathml
-If you use it on a webpage, please send the URL to jipsen@chapman.edu
-
-Copyright (c) 2014 Peter Jipsen and other ASCIIMathML.js contributors
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
+ *  Copyright (c) 2014 Peter Jipsen and other ASCIIMathML.js contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ */
 var asciimath = {};
 
 (function(){
