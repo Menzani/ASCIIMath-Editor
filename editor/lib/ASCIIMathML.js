8b2df00ab3eae1b879b0e720223cd667b56e14e9
Francesco Menzani <francesco@menzani.eu>
2017-12-03 19:29:44 +0100

    Remove auto math recognize feature

--- editor/lib/ASCIIMathML.js
+++ editor/lib/ASCIIMathML.js
@@ -20,7 +20,6 @@ var asciimath = {};
     var mathfontsize = "1em"      // change to e.g. 1.2em for larger math
     var mathfontfamily = "serif"  // change to "" to inherit (works in IE)
     // or another family (e.g. "arial")
-    var automathrecognize = false // writing "amath" on page makes this true
     var checkForMathML = true     // check if browser can display MathML
     var notifyIfNoMathML = true   // display note at top if no MathML capability
     var alertIfNoMathML = false   // show alert box if no MathML capability
@@ -1080,37 +1079,6 @@ Each terminal symbol is translated into a corresponding mathml node.*/
         return newFrag
     }
 
-    function AMautomathrec(str) {
-//formula is a space (or start of str) followed by a maximal sequence of *two* or more tokens, possibly separated by runs of digits and/or space.
-//tokens are single letters (except a, A, I) and ASCIIMathML tokens
-        var texcommand = "\\\\[a-zA-Z]+|\\\\\\s|"
-        var ambigAMtoken = "\\b(?:oo|lim|ln|int|oint|del|grad|aleph|prod|prop|sinh|cosh|tanh|cos|sec|pi|tt|fr|sf|sube|supe|sub|sup|det|mod|gcd|lcm|min|max|vec|ddot|ul|chi|eta|nu|mu)(?![a-z])|"
-        var englishAMtoken = "\\b(?:sum|ox|log|sin|tan|dim|hat|bar|dot)(?![a-z])|"
-        var secondenglishAMtoken = "|\\bI\\b|\\bin\\b|\\btext\\b" // took if and or not out
-        var simpleAMtoken = "NN|ZZ|QQ|RR|CC|TT|AA|EE|sqrt|dx|dy|dz|dt|xx|vv|uu|nn|bb|cc|csc|cot|alpha|beta|delta|Delta|epsilon|gamma|Gamma|kappa|lambda|Lambda|omega|phi|Phi|Pi|psi|Psi|rho|sigma|Sigma|tau|theta|Theta|xi|Xi|zeta" // uuu nnn?
-        var letter = "[a-zA-HJ-Z](?=(?:[^a-zA-Z]|$|" + ambigAMtoken + englishAMtoken + simpleAMtoken + "))|"
-        var token = letter + texcommand + "\\d+|[-()[\\]{}+=*&^_%\\\@/<>,\\|!:;'~]|\\.(?!(?:\x20|$))|" + ambigAMtoken + englishAMtoken + simpleAMtoken
-        var re = new RegExp("(^|\\s)(((" + token + ")\\s?)((" + token + secondenglishAMtoken + ")\\s?)+)([,.?]?(?=\\s|$))", "g")
-        str = str.replace(re, " `$2`$7")
-        var arr = str.split(AMdelimiter1)
-        var re1 = new RegExp("(^|\\s)([b-zB-HJ-Z+*<>]|" + texcommand + ambigAMtoken + simpleAMtoken + ")(\\s|\\n|$)", "g")
-        var re2 = new RegExp("(^|\\s)([a-z]|" + texcommand + ambigAMtoken + simpleAMtoken + ")([,.])", "g") // removed |\d+ for now
-        for (i = 0; i < arr.length; i++)   //single nonenglish tokens
-            if (i % 2 == 0) {
-                arr[i] = arr[i].replace(re1, " `$2`$3")
-                arr[i] = arr[i].replace(re2, " `$2`$3")
-                arr[i] = arr[i].replace(/([{}[\]])/, "`$1`")
-            }
-        str = arr.join(AMdelimiter1)
-        str = str.replace(/((^|\s)\([a-zA-Z]{2,}.*?)\)`/g, "$1`)")  //fix parentheses
-        str = str.replace(/`(\((a\s|in\s))(.*?[a-zA-Z]{2,}\))/g, "$1`$3")  //fix parentheses
-        str = str.replace(/\sin`/g, "` in")
-        str = str.replace(/`(\(\w\)[,.]?(\s|\n|$))/g, "$1`")
-        str = str.replace(/`([0-9.]+|e.g|i.e)`(\.?)/gi, "$1$2")
-        str = str.replace(/`([0-9.]+:)`/g, "$1")
-        return str
-    }
-
     function processNodeR(n, linebreaks, latex) {
         var mtch, str, arr, frg, i
         if (n.childNodes.length == 0) {
@@ -1138,22 +1106,7 @@ Each terminal symbol is translated into a corresponding mathml node.*/
                                 mtch = true
                                 return "AMescape1"
                             })
-                        str = str.replace(/\\?end{?a?math}?/i,
-                            function () {
-                                automathrecognize = false
-                                mtch = true
-                                return ""
-                            })
-                        str = str.replace(/amath\b|\\begin{a?math}/i,
-                            function () {
-                                automathrecognize = true
-                                mtch = true
-                                return ""
-                            })
                         arr = str.split(AMdelimiter1)
-                        if (automathrecognize)
-                            for (i = 0; i < arr.length; i++)
-                                if (i % 2 == 0) arr[i] = AMautomathrec(arr[i])
                         str = arr.join(AMdelimiter1)
                         arr = str.split(AMdelimiter1)
                         for (i = 0; i < arr.length; i++) // this is a problem ************
@@ -1189,7 +1142,7 @@ Each terminal symbol is translated into a corresponding mathml node.*/
             } catch (err) {
             }
 //alert(st)
-            if (st == null || /amath\b|\\begin{a?math}/i.test(st) ||
+            if (st == null ||
                 st.indexOf(AMdelimiter1 + " ") != -1 || st.slice(-1) == AMdelimiter1 ||
                 st.indexOf(AMdelimiter1 + "<") != -1 || st.indexOf(AMdelimiter1 + "\n") != -1) {
                 processNodeR(n, linebreaks, false)
